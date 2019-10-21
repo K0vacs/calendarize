4b608cb04ae36b6272532936248964ac0c3dee17
Ferenc Kovacs <mr.f.kovacs@gmail.com>
2019-10-21 17:08:05 +0200

    settings.py updated to handle mysql database

--- app/main/settings.py
+++ app/main/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql_db',
+        'USER': 'default',
+        'PASSWORD': 'secret',
+        'HOST': 'db',
+        'PORT': 3306,
     }
 }
 
