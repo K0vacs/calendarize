1b2ddeb1f893929841a0275e9ff270bbe61ac0ec
Ferenc Kovacs <mr.f.kovacs@gmail.com>
2020-05-18 08:51:20 +0200

    Fixed redirect on login page

--- app/main/urls.py
+++ app/main/urls.py
@@ -3,12 +3,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from . import views
 
 # These are all the urls for the application
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    path('accounts/', include('django.contrib.auth.urls')), 
     path('', views.HomePage.as_view(), name='home'),
     path('payment/<message>', views.PaymentSubmission.as_view(), name='payment'),
     path('contact/<message>', views.FormSubmission.as_view(), name='contact'),
